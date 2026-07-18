44d5078cb9c50ba79b057ac7ae36df0032d7b697
derickshanana <derickshanana50@gmail.com>
2026-07-18 20:51:01 +0200

    Hello World

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('CSE 341 Contacts API is running');
+  res.send('Hello World');
 });
 
 router.use('/contacts', require('./contacts'));
